337e4e151f310a415f71e08ce3f86cbc1c4f1268
ByteDance <alexdelgadodesign@gmail.com>
2026-01-14 16:16:11 -0800

    Add navbar from Figma design to media crop catalog manager

--- apps/media_crop_catalog_manager/src/App.jsx
+++ apps/media_crop_catalog_manager/src/App.jsx
@@ -1,18 +1,13 @@
 import BackToLauncher from './components/BackToLauncher'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <BackToLauncher />
-      <div className="pt-[40px]">
-        <div className="container mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold text-gray-800 mb-4">
-            Media Crop Catalog Manager
-          </h1>
-          <p className="text-lg text-gray-600">
-            Adding crop functionality to media files on catalog manager
-          </p>
-        </div>
+      <Navbar />
+      <div className="pt-[104px]">
+        {/* App content will go here */}
       </div>
     </div>
   )
